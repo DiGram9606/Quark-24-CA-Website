ed9688f8d1ff559ec75aae432b12187c9fd38adf
Harshvardhan Mestha <harshvardhanmestha@yahoo.com>
2023-09-29 22:05:57 +0530

    Added Sheets API to Registration Page.

--- src/Components/Registration.js
+++ src/Components/Registration.js
@@ -2,6 +2,7 @@ import React from "react"
 import "./Registration.css"
 import App from "../App.js"
 import { BrowserRouter, Link, Routes, Route, useNavigate } from "react-router-dom";
+
 export default function Registration(){
     let [formData, updateFormData]=React.useState({
         name:"",
@@ -25,6 +26,55 @@ export default function Registration(){
 
     function handleSubmit(event){
         console.log(formData)
+        const date = new Date();
+        var ISToffSet = 330; //IST is 5:30; i.e. 60*5+30 = 330 in minutes 
+        var ISTTime = new Date(date.getTime()+ISToffSet*60*1000);
+    
+    
+        // if (
+        //   formData.fullName !== "" && formData.email !== "" && formData.phone !== "" && formData.gender !== "" && formData.college !== "" && formData.city !== "" && formData.state !== "" && isTeamShowing
+        //     ? !team.slice(0, 2).some((eachMemberObj) => eachMemberObj.n === "")
+        //     : true && personName?.length !== 0
+        // ) {
+        //   if (!validator.isEmail(formData.email)) {
+        //     alert("Please Enter Your Email.");
+        //     return;
+        //   } else if (!validator.isMobilePhone(formData.phone, "en-IN")) {
+        //     alert("Please enter a valid phone number!");
+        //     return;
+        //   }
+
+    
+
+    
+        let usr_data = {
+            Submit_Time: ISTTime,
+            Name: formData.name,
+            Email: formData.email,
+            Mobile_No: formData.mobile,
+            College_Name: formData.college,
+            Personal_Address:formData.personalAddress,
+            College_Address:formData.collegeAddress,
+        };
+    
+
+        fetch("https://sheet.best/api/sheets/f2876a3c-f7ef-465b-b5ef-283934de8117", {
+            method: "POST",
+            mode: "cors",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify(usr_data),
+        })
+            .then((r) => r.json())
+            .then((data) => {
+            // The response comes here
+            console.log(data);
+            })
+            .catch((error) => {
+            // Errors are reported there
+            console.log(error);
+            });
     }
 
     return(
